827b8df4149ff8af9bf2e6f57ee0f66f52ca8751
CriDos <CriDos@ya.ru>
2019-01-12 13:52:17 +0300

    release_1.0.9

--- AsTaskTests/Properties/AssemblyInfo.cs
+++ AsTaskTests/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HardDev")]
 [assembly: AssemblyProduct("AsTaskTests")]
-[assembly: AssemblyCopyright("Copyright HardDev©  2018")]
+[assembly: AssemblyCopyright("Copyright HardDev©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8")]
-[assembly: AssemblyFileVersion("1.0.8")]
+[assembly: AssemblyVersion("1.0.9")]
+[assembly: AssemblyFileVersion("1.0.9")]
